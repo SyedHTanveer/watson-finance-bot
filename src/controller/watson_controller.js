bcac3d295cf23853ad40ac1fc297820a7e18ed40
faustino99 <faustino.cortina.21@dartmouth.edu>
2020-05-27 17:26:03 -0400

    biggest movers and biggest losers added

--- src/controller/watson_controller.js
+++ src/controller/watson_controller.js
@@ -13,15 +13,8 @@ export const handleQuestion = (req, res) => {
           response.pch = result.data.ticker.todaysChangePerc;
           response.volume = result.data.ticker.day.v;
           response.price = result.data.ticker.lastQuote.p;
-          res.json(response);
+          return axios.get(`https://api.polygon.io/v1/meta/symbols/AAPL/news?perpage=5&apiKey=${process.env.POLYGON_API_KEY}`);
         })
-        .catch((error) => {
-          console.log(error);
-          res.status(500).json(error);
-        });
-      break;
-    case 'news':
-      axios.get(`https://api.polygon.io/v1/meta/symbols/AAPL/news?perpage=5&apiKey=${process.env.POLYGON_API_KEY}`)
         .then((result) => {
           result.data.forEach((newsArticle, i) => {
             response[`headline${i}`] = newsArticle.title;
@@ -29,6 +22,40 @@ export const handleQuestion = (req, res) => {
             response[`source${i}`] = newsArticle.source;
             response[`image${i}`] = newsArticle.image;
           });
+          res.json(response);
+        })
+        .catch((error) => {
+          console.log(error);
+          res.status(500).json(error);
+        });
+      break;
+    case 'biggest-movers':
+      axios.get(`https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/gainers?apiKey=${process.env.POLYGON_API_KEY}`)
+        .then((result) => {
+          result.data.tickers.forEach((stock, i) => {
+            if (i < 10) {
+              response[`stock${i}`] = stock.ticker;
+              response[`pch${i}`] = stock.todaysChangePerc;
+            }
+          });
+          // TODO: add sentiment biggest gainers here
+          res.json(response);
+        })
+        .catch((error) => {
+          console.log(error);
+          res.status(500).json(error);
+        });
+      break;
+    case 'biggest-losers':
+      axios.get(`https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/losers?apiKey=${process.env.POLYGON_API_KEY}`)
+        .then((result) => {
+          result.data.tickers.forEach((stock, i) => {
+            if (i < 10) {
+              response[`stock${i}`] = stock.ticker;
+              response[`pch${i}`] = stock.todaysChangePerc;
+            }
+          });
+          // TODO: add sentiment biggest gainers here
           res.json(response);
         })
         .catch((error) => {
