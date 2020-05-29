f23c4ef700089ec4a7673082b32b94d37bfdc2fb
faustino99 <faustino.cortina.21@dartmouth.edu>
2020-05-29 18:58:11 -0400

    made percent change reflect a percentage w two decimal places

--- src/controller/watson_controller.js
+++ src/controller/watson_controller.js
@@ -10,7 +10,7 @@ export const handleQuestion = (req, res) => {
     case 'stock':
       axios.get(`https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers/${req.body.ticker}?apiKey=${process.env.POLYGON_API_KEY}`)
         .then((result) => {
-          response.pch = result.data.ticker.todaysChangePerc;
+          response.pch = (result.data.ticker.todaysChangePerc * 100).toFixed(2);
           response.volume = result.data.ticker.day.v;
           response.price = result.data.ticker.lastQuote.p;
           return axios.get(`https://api.polygon.io/v1/meta/symbols/AAPL/news?perpage=5&apiKey=${process.env.POLYGON_API_KEY}`);
